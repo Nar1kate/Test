573088ce8c673c98088de946376dca6aee0ffbfd
Narikate <samolofav.zhenya@mail.ru>
2024-07-07 17:33:22 +0300

    решение какой-то чукчи

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-for i in range(10):
-    print('hello github')
-    if i == 5:
-        break
+n = int(input())
+m = int(input())
+k1 = int(input())
+k2 = int(input())
+
+variance = k1 - k2
+
+bol = k1 - m
+
+perc = bol / variance
+
+print(n - int(n * perc), int(n * perc))
